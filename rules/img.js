e226ca1883fa246ac3bd4184be943d572b69e111
zhanglei@dxyer.com <zhanglei@dxyer.com>
2016-08-14 17:34:54 +0800

    fix cann't get img size

--- rules/img.js
+++ rules/img.js
@@ -31,30 +31,6 @@ module.exports = {
         }
     },
     attrRule: {
-        src: function (key, $child, root, collectError, rule, style, ops) {
-            var display = style['display'];
-            if (display == 'inline-block' || !display) {
-                collectError['img'] = '请注意img处于inline-block的时候,会造成父元素大于图片的高度';
-            }
-            var attr = $child.attr(key);
-            if (attr) {
-                if (!Util.isUrl(attr)) {
-                    collectError['img_src'] = '请注意img的地址是相对地址,请改成绝对地址';
-                    if (ops.CDN) {
-                        $child.attr('src', url.resolve(ops.CDN, attr));
-                        collectError['img_src_auto'] = '已自动修改路径为CDN路径';
-                    }
-                }
-            }
-
-        },
-        border: function (key, $child, root, collectError, rule, style) {
-            var attr = $child.attr(key);
-            if (!attr) {
-                $child.attr(key, 0);
-                collectError['img_border'] = '请注意添加img的border(默认0)';
-            }
-        },
         width: function (key, $child, root, collectError, rule, style, ops) {
             var attr = $child.attr(key);
             if (!attr && !style.width) {
@@ -73,6 +49,32 @@ module.exports = {
             } else if (!attr && style.height) {
                 collectError['img_attr_height'] = 'img高最好写在属性上';
                 $child.attr(key, style.height.replace('px', ''));
+            }
+        },
+        /* src 要放在 width 和 height 后面, 因为 src 在 存在 CDN 的情况下会改写 url, 导致获取不到本地的图片大小*/
+        src: function (key, $child, root, collectError, rule, style, ops) {
+            var display = style['display'];
+            if (display == 'inline-block' || !display) {
+                collectError['img'] = '请注意img处于inline-block的时候,会造成父元素大于图片的高度';
+            }
+            var attr = $child.attr(key);
+            if (attr) {
+                if (!Util.isUrl(attr)) {
+                    collectError['img_src'] = '请注意img的地址是相对地址,请改成绝对地址';
+                    if (ops.CDN) {
+                        $child.attr('src', url.resolve(ops.CDN, attr));
+                        collectError['img_src_auto'] = '已自动修改路径为CDN路径';
+
+                    }
+                }
+            }
+
+        },
+        border: function (key, $child, root, collectError, rule, style) {
+            var attr = $child.attr(key);
+            if (!attr) {
+                $child.attr(key, 0);
+                collectError['img_border'] = '请注意添加img的border(默认0)';
             }
         },
         alt: function (key, $child, root, collectError, rule, style) {
